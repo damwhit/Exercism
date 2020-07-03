c92873a9d78d23ed9c7ee92458f5bbfeb9f0dbf2
David Whitaker <damwhit@gmail.com>
2020-07-03 10:56:30 -0600

    Refactor simple linked list for ruby

--- ruby/simple-linked-list/simple_linked_list.rb
+++ ruby/simple-linked-list/simple_linked_list.rb
@@ -8,18 +8,15 @@ class Element
   end
 end
 
-
 class SimpleLinkedList
-  attr_accessor :head, :current_node
-
   def initialize(data = [])
     @head = nil
     @current_node = nil
     @arr = []
     data = data.to_a if data.class == Range
-    if !data.empty?
-      data.map { |e| self.push(Element.new(e)) }
-    end
+    return if data.empty?
+
+    data.map { |e| push(Element.new(e)) }
   end
 
   def push(element)
@@ -31,46 +28,46 @@ class SimpleLinkedList
 
   def pop
     return nil if @head.nil?
+
     @current_node = @head
     until @current_node.next.nil?
       prev_node = @current_node
       @current_node = @current_node.next
     end
     prev_node.next = nil if prev_node
-    return @current_node
+    @current_node
   end
 
   def to_a
-    if !@head.nil?
-      traverse(true)
-    end
+    traverse(true) unless @head.nil?
     @arr
   end
 
   def reverse!
     @current_node = @head
     previous_node = nil
-    while !@current_node.nil?
+    until @current_node.nil?
       next_node = @current_node.next
       @current_node.next = previous_node
       previous_node = @current_node
-      @head = @current_node if next_node == nil
+      @head = @current_node if next_node.nil?
       @current_node = next_node
     end
     self
   end
 
   private
-    def traverse(should_add = false)
-      @current_node = @head
-      while !@current_node.next.nil?
-        add if should_add
-        @current_node = @current_node.next
-      end
-      add if @current_node.next.nil? && should_add
-    end
 
-    def add
-      @arr.unshift @current_node.datum
+  def traverse(should_add = false)
+    @current_node = @head
+    until @current_node.next.nil?
+      add if should_add
+      @current_node = @current_node.next
     end
+    add if @current_node.next.nil? && should_add
+  end
+
+  def add
+    @arr.unshift @current_node.datum
+  end
 end
