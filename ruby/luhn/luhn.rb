deae28d91cc280f90fe3e0efdd9fb92b3d2a4bbf
David Whitaker <damwhit@gmail.com>
2020-08-02 19:42:15 -0600

    Add luhn for ruby

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Luhn' exercise in this file. Make the tests in
-`luhn_test.rb` pass.
+class Luhn
+  def self.valid?(string)
+    string.gsub!(/\s+/, "")
+    return false if !string.scan(/\D/).empty?
 
-To get started with TDD, see the `README.md` file in your
-`ruby/luhn` directory.
-=end
+    numbers = string.delete('^0-9')
+    return false if numbers.length <= 1
 
+    sum = numbers.reverse.split('').each_with_index.map do |num, i|
+      num = num.to_i
+      if i.odd?
+        num = num * 2
+        num -= 9 if num > 9
+      end
+      num
+    end.reduce(:+)
+    sum % 10 == 0
+  end
+end
