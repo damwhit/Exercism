d94b5c5ae25309b30db263cd28e81846dd0c7cbe
David Whitaker <damwhit@gmail.com>
2022-02-02 08:07:08 -0500

    Finish Bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -4,6 +4,19 @@
 // convenience to get you started writing code faster.
 //
 
-export const hey = (message) => {
-  throw new Error("Remove this statement and implement this function");
+export const hey = message => {
+  message = message.trim();
+  const isYelling = message.toUpperCase() === message && message.match(/[a-z]/i);
+  const isAsking = message[message.length - 1] === '?'
+  if (isYelling && isAsking) {
+    return 'Calm down, I know what I\'m doing!';
+  } else if (isYelling) {
+    return 'Whoa, chill out!';
+  } else if (isAsking) {
+    return 'Sure.';
+  } else if (message.length === 0) {
+    return 'Fine. Be that way!';
+  } else {
+    return 'Whatever.';
+  }
 };
--- javascript/bob/bob.spec.js
+++ javascript/bob/bob.spec.js
@@ -43,7 +43,7 @@ describe('Bob', () => {
 
   xtest('forceful question', () => {
     const result = hey('WHAT THE HELL WERE YOU THINKING?');
-    expect(result).toEqual('Calm down, I know what I\'m doing!');
+    expect(result).toEqual("Calm down, I know what I'm doing!");
   });
 
   xtest('shouting numbers', () => {
@@ -51,12 +51,12 @@ describe('Bob', () => {
     expect(result).toEqual('Whoa, chill out!');
   });
 
-  xtest('only numbers', () => {
+  xtest('no letters', () => {
     const result = hey('1, 2, 3');
     expect(result).toEqual('Whatever.');
   });
 
-  xtest('question with only numbers', () => {
+  xtest('question with no letters', () => {
     const result = hey('4?');
     expect(result).toEqual('Sure.');
   });
